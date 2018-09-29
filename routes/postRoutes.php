6cb481976123273c4f4d3e8015f17c92d3eae08c
Dylan <dylan.baine@yahoo.com>
2018-09-29 16:03:31 -0500

    fix creat post backend route

--- routes/postRoutes.php
+++ routes/postRoutes.php
@@ -1,8 +1,12 @@
 <?php
+Route::get('{type}/create', function($type){
+    return response()->json([
+        'type' => \App\Models\Type::where('model', 'Post')->where('slug', $type)->with('posts')->first()
+    ]);
+});
 Route::get('{type}/{post}/edit', 'PostController@edit');
 Route::resource('posts', 'PostController');
 Route::get('{type}', function($type){
     return \App\Models\Type::where('model', 'Post')->where('slug', $type)->with('posts')->first();
 });
-Route::get('{type}/{post}', 'PostController@show');
 
